c2e71681ccb85c2046fadd90f69b51849ccad9a9
Andro Babu <rambabusaravanan@gmail.com>
2020-12-29 16:47:05 +0800

    scripts

---
+++ clean.sh
@@ -0,0 +1,1 @@
+rm -r bookit.zip bookit-master _data _includes assets .eleventy.js .gitignore
---
+++ pull.sh
@@ -0,0 +1,3 @@
+curl -s -o bookit.zip https://codeload.github.com/rambabusaravanan/bookit/zip/master
+unzip bookit.zip
+cp -vr bookit-master/.* .
