2387f591c3dd0f06145450988a048fc2809fabc7
Andro Babu <rambabusaravanan@gmail.com>
2020-12-29 16:29:00 +0800

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,51 @@
+function toTitle(value) {
+  let splits = value.replace(/[-_]/g, ' ').replace(/^\//g, '').replace(/(\/|\/index\.html)$/, '').split('/')
+
+  if (splits.length === 1) return splits[0];
+  return splits.join(' » ')
+}
+
+function toNavTree (list) {
+  let output = [], map = {};
+
+  list.forEach(it => {
+    let splits = it.replace(/^\//, '').replace(/(\/|\/index\.html)$/, '').split('/');
+
+    splits.reduce(function(prevLink, s, i) {
+      let currLink = prevLink ? `${prevLink}/${s}` : s;
+      if (!map[currLink]) {
+        map[currLink] = { text: s.replace(/[-_]/g, ' ') }
+
+        // if root, add to output; else, add to map
+        if (!prevLink) {
+          output.push(map[currLink])
+        } else if (prevLink && map[prevLink]) {
+          if (!map[prevLink].child) map[prevLink].child = []
+          map[prevLink].child.push(map[currLink]);
+        } 
+      }
+
+      // if last, add 'link'
+      if (i === splits.length - 1)
+        map[currLink].link = it;
+      
+      // return for next accumulator
+      return currLink
+    }, '')
+  })
+  return output;
+}
+
+module.exports = function (eleventyConfig) {
+  // Values can be static:
+  // eleventyConfig.addGlobalData("static", "static");
+
+  eleventyConfig.addFilter("toTitle", toTitle);
+
+  eleventyConfig.addCollection("navTree", function (collectionApi) {
+    const urlList = collectionApi.getAll().map(it => it.url)    
+    return toNavTree(urlList);
+  });
+
+  eleventyConfig.addPassthroughCopy("assets/style.css");
+};
