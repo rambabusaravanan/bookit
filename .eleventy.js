883d9c99f3db73bfb7a6556bae9f563012abb9c8
Andro Babu <rambabusaravanan@gmail.com>
2020-12-30 14:23:39 +0800

    primer md css; sort links; Home text

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
+const SITE_HOME = 'Home'
+
 function toTitle(value) {
+  if (value === '/') return SITE_HOME;
+
   let splits = value.replace(/[-_]/g, ' ').replace(/^\//g, '').replace(/(\/|\/index\.html)$/, '').split('/')
 
   if (splits.length === 1) return splits[0];
@@ -14,7 +18,7 @@ function toNavTree (list) {
     splits.reduce(function(prevLink, s, i) {
       let currLink = prevLink ? `${prevLink}/${s}` : s;
       if (!map[currLink]) {
-        map[currLink] = { text: s.replace(/[-_]/g, ' ') }
+        map[currLink] = { text: it === '/' ? SITE_HOME : s.replace(/[-_]/g, ' ') }
 
         // if root, add to output; else, add to map
         if (!prevLink) {
@@ -43,7 +47,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("toTitle", toTitle);
 
   eleventyConfig.addCollection("navTree", function (collectionApi) {
-    const urlList = collectionApi.getAll().map(it => it.url)    
+    const urlList = collectionApi.getAll().map(it => it.url).sort()
     return toNavTree(urlList);
   });
 
